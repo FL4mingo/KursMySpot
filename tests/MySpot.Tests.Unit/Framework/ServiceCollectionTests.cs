ece42db11368a5bed159aa3841f4041780b702e6
FL4mingo <maciej.chudzinski@icloud.com>
2024-03-18 22:27:22 +0100

    Zapoznanie się z typami serwisów przy DependencyInjection

---
+++ tests/MySpot.Tests.Unit/Framework/ServiceCollectionTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace MySpot.Tests.Unit.Framework;
+
+public class ServiceCollectionTests
+{
+    [Fact]
+    public void test()
+    {
+        var serviceColletion = new ServiceCollection();
+
+        serviceColletion.AddSingleton<IMessenger,MessengerSingleton>();
+        serviceColletion.AddTransient<IMessenger,MessengerTransient>();
+        serviceColletion.AddScoped<IMessenger,MessengerScoped>();
+
+        var serviceProvider = serviceColletion.BuildServiceProvider();
+
+        var messengers = serviceProvider.GetServices<IMessenger>();
+
+
+        using(var scope = serviceProvider.CreateScope())
+        {
+            var messangers = scope.ServiceProvider.GetServices<IMessenger>();
+
+        }
+    }
+
+    private interface IMessenger
+    {
+        void Send();
+    }
+
+    private class MessengerSingleton : IMessenger
+    {
+        private readonly Guid _id = Guid.NewGuid();
+
+        public void Send() => Console.WriteLine($"Sending a message... [{_id}]");
+    }
+    private class MessengerTransient : IMessenger
+    {
+        private readonly Guid _id = Guid.NewGuid();
+
+        public void Send() => Console.WriteLine($"Sending a message... [{_id}]");
+    }
+    private class MessengerScoped : IMessenger
+    {
+        private readonly Guid _id = Guid.NewGuid();
+
+        public void Send() => Console.WriteLine($"Sending a message... [{_id}]");
+    }
+}
